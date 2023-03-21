ab531d18aa62d6f27ddf46d2eb2027f9466decdf
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-03-21 12:59:32 +0000

    Add links

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <a
           href="https://github.com/Tetiana1302/weather-app-react"
           target="_blank"
+          rel="noopener noreferrer"
         >
           {" "}
           open-sourced on GidHub
