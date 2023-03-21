72d9c2766af77c543fbf16a8bea86e5cd27bbe6b
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-03-21 13:20:27 +0000

    Added bootatrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Tetiana Bysykalo and is
-        <a
-          href="https://github.com/Tetiana1302/weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          open-sourced on GidHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Tetiana Bysykalo and is
+          <a
+            href="https://github.com/Tetiana1302/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on GidHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
