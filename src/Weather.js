170fcbdd84accc39843d1af17930f41f472d7ca9
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-03-23 18:19:22 +0000

    Added basic staly
    C

--- src/Weather.js
+++ src/Weather.js
@@ -11,29 +11,37 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-light w-100"
+            />
           </div>
         </div>
       </form>
       <h1>London</h1>
-      <div className="row">
-        <div className="col-6">
+      <div className="row mt-3">
+        <div className="col-sm-6">
           <ul>
             <li>Tuesday 11-00</li>
-            <li>Overcast Clouds</li>
+            <li className="text-capitalize">Overcast Clouds</li>
             <li>Humidity: 91%, Wind: 5 km/h</li>
           </ul>
         </div>
-        <div className="col-6">
-          <img
-            src="https://m.netinfo.bg/sinoptik/icons/small/d300.png"
-            alt="cloudy"
-          ></img>
-          <span>7</span>
-          <span>ºC</span>
+        <div className="col-sm-6">
+          <div className="temperature-container d-flex justify-content-center align-items-stretch">
+            <img
+              src="https://m.netinfo.bg/sinoptik/icons/small/d300.png"
+              alt="cloudy"
+              className="float-left"
+            ></img>
+            <span className="temperature">7</span>
+            <span className="unit">ºC</span>
+          </div>
         </div>
       </div>
     </div>
